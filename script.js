983612293a0abec08b15de5e347c93543bedf7d2
Naya <anayahfathur@gmail.com>
2025-04-22 11:21:21 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const menubar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu-navigation");
+
+menubar.addEventListener("click", function() {
+    // Toggle menu-active untuk menunjukkan menu
+    menuNav.classList.toggle("menu-active");
+
+    // Toggle clicked untuk animasi hamburger
+    menubar.classList.toggle("clicked");
+
+    // Untuk melihat apakah JavaScript berfungsi, bisa log ke konsol
+    console.log("Hamburger menu clicked!");
+});
+
+// clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
